eedb0235947e7bb4fe4fe25c34b6c1b667614d9e
Thashin Naidoo <Thashin@live.com>
2018-06-27 09:00:58 +0800

    Initial commit

---
+++ MyProject/Controllers/ReverseWordsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyProject.Controllers
+{
+    public class ReverseWordsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "" };
+        }
+
+        // GET api/values/5
+        public string Get([FromUri] string sentence)
+        {
+
+            return string.Join(" ", sentence
+                .Split(' ')
+                .Select(x => new string(x.Reverse().ToArray())));
+        }
+
+    }
+}
