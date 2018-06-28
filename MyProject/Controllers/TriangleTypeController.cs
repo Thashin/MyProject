29001802c6d1f06342e1449458db8a76c0fa4b79
Thashin Naidoo <Thashin@live.com>
2018-06-28 13:01:04 +0800

    Triangle comments

--- MyProject/Controllers/TriangleTypeController.cs
+++ MyProject/Controllers/TriangleTypeController.cs
@@ -15,37 +15,48 @@ namespace MyProject.Controllers
         private const string scalene = "Scalene";
 
 
-        
+
         public HttpResponseMessage Get([FromUri] int a, [FromUri] int b, [FromUri] int c)
         {
-           
-                if (a <= 0 || b <= 0 || c <= 0)
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, error);
-                }
 
-                int[] sides = new int[] { a, b, c };
+            return Request.CreateResponse(HttpStatusCode.OK, DetermineType(a, b, c));
+        }
 
-                Array.Sort(sides);
+        ///<summary> Determines the type of triangle based on the length of the sides
+        ///There are two error cases in to look for
+        /// 1) a negative length
+        /// 2) the value of the longest side is less than or equal to the sum of the other 2 sides
+        ///</summary>
 
-                if (sides[0] + sides[1] <= sides[2])
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, error);
-                }
-                if (sides.Distinct().Count() == 1) 
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, equilateral);
-                }
-                if (sides.Distinct().Count() == 2) 
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, isosceles);
-                }
-                if (sides.Distinct().Count() == 3) 
-                {
-                    return Request.CreateResponse(HttpStatusCode.OK, scalene);
-                }
-            
-            return Request.CreateResponse(HttpStatusCode.OK, error);
+        private string DetermineType(int a,int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                return error;
+            }
+
+            int[] sides = new int[] { a, b, c };
+
+            Array.Sort(sides);
+
+            if (sides[0] + sides[1] <= sides[2])
+            {
+                return error;
+            }
+            if (sides.Distinct().Count() == 1)
+            {
+                return equilateral;
+            }
+            if (sides.Distinct().Count() == 2)
+            {
+                return isosceles;
+            }
+            if (sides.Distinct().Count() == 3)
+            {
+                return scalene;
+            }
+
+            return error;
         }
 
     }
