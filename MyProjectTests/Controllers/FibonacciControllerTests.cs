b7b32bcde62e11adc34147aa00e6bfa7db619ac7
Thashin Naidoo <Thashin@live.com>
2018-06-28 10:52:50 +0800

    Fib Tests

--- MyProjectTests/Controllers/FibonacciControllerTests.cs
+++ MyProjectTests/Controllers/FibonacciControllerTests.cs
@@ -3,24 +3,60 @@ using MyProject.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace MyProject.Controllers.Tests
 {
     [TestClass()]
     public class FibonacciControllerTests
     {
-        [TestMethod()]
-        public void GetTest()
+        public FibonacciController sut;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            Assert.Fail();
+            sut = new FibonacciController()
+            {
+                Request = new HttpRequestMessage(),
+                Configuration = new HttpConfiguration()
+            };
         }
 
         [TestMethod()]
-        public void GetTest1()
+        public void BaseNumberTest()
         {
-            Assert.Fail();
+            long expected = 1;
+            sut.Get(1).TryGetContentValue(out long actual);
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void PositiveNumberTest()
+        {
+            long expected = 6765;
+            sut.Get(20).TryGetContentValue(out long actual);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod()]
+        public void NegativeNumberTest()
+        {
+            long expected = -102334155;
+            sut.Get(-40).TryGetContentValue(out long actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LargeNumberTest()
+        {
+            var expected = HttpStatusCode.BadRequest;
+            var actual = sut.Get(94).StatusCode;
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
